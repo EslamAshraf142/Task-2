aa6dc608ba058a2e5b4ad2bbadf9cf4546003869
EslamAshraf142 <84976734+EslamAshraf142@users.noreply.github.com>
2022-08-10 17:27:42 +0200

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def binary_search(arr, a, low, high):
+
+    if high >= low:
+
+        mid = low + (high - low)//2
+
+        # If found at mid, then return it
+        if arr[mid] == a:
+            return mid
+
+        # Search the left half
+        elif arr[mid] > a:
+            return binary_search(arr, a, low, mid-1)
+
+        # Search the right half
+        else:
+            return binary_search(arr, a, mid + 1, high)
+
+    else:
+        return -1
+
+# printing array and element to be found
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("The array given is: ", arr)
+a = 9
+print("The element to be found is: ", a)
+
+index = binary_search(arr, a, 0, len(arr)-1)
+
+if index != -1:
+    print("Element is present at index " + str(index))
+else:
+    print("Not found")
+
+
+# In[6]:
+
+
+def tri_recursion(k):
+  if(k>0):
+    result = k+tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
+# In[ ]:
+
+
+
+
